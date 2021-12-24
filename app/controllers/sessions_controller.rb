f2699fc098a62c63ddac78ff18b9f333ca3fb837
jack <whisler1@gmail.com>
2021-12-24 14:39:29 -0600

    implements current user logic on welcome page. shows name of user and option to logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,24 @@
 class SessionsController < ApplicationController
+  def new
+    
+  end
+  
   def create
     @user = User.find_by(email: params[:email])
     #authenticate user credentials
     if !!@user && @user.authenticate(params[:password])
       #set session and redirect on success
       session[:user_id] = @user.id
-      redirect_to user_path
+      redirect_to root_path
     else
       #error message on fail
       message = "Something went wrong. Make sure email and password are correct."
-      redirect_to login_path, notice: message
+      redirect_to sign_in_path, notice: message
     end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to sign_in_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  get 'breaks/index'
-  get "/organizations" => "organizations#index"
+  get 'welcome', to: 'welcome#index'
+  root to: 'welcome#index'
+  
   resources :organizations
   # Users routes
   resources :users, only: [:new, :create, :edit, :update, :show, :destroy]
-  # get "/users/new" => "users#new"
-  # post "/users" => "users#create"
-  
+  get "/sign_up" => "users#new"
+  post "/sign_up" => "users#create"
+
   # Sessions routes
-  get "/login" => "sessions#login"
-  post "/login" => "sessions#create"
-  post "/logout" => "sessions#destroy"
-  get "/logout" => "sessions#destroy"
+  get "/sign_in" => "sessions#new"
+  post "/sign_in" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
 
 end
